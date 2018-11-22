bfe9bd25748ad2b1d828ccbc44e40e86542f8536
Oki Wicaksono <okiwicaksono@gmail.com>
2018-11-22 21:03:43 +0700

    fix service

--- app/src/main/java/com/dicoding/finalsoccermatches/external/api/MatchService.kt
+++ app/src/main/java/com/dicoding/finalsoccermatches/external/api/MatchService.kt
@@ -4,18 +4,18 @@ import com.dicoding.finalsoccermatches.domain.entity.MatchResponse
 import com.dicoding.finalsoccermatches.domain.entity.TeamResponse
 import kotlinx.coroutines.experimental.Deferred
 import retrofit2.http.GET
-import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface MatchService {
-    @GET ("api/v1/json/1/eventspastleague.php?id=4328")
+    @GET("api/v1/json/1/eventspastleague.php?id=4328")
     fun getPastMatches(): Deferred<MatchResponse>
 
-    @GET ("api/v1/json/1/eventsnextleague.php?id=4328")
+    @GET("api/v1/json/1/eventsnextleague.php?id=4328")
     fun getNextMatches(): Deferred<MatchResponse>
 
-    @GET ("api/v1/json/1/lookupteam.php?id={id}")
-    fun getTeamBadge(@Path("id") id: String): Deferred<TeamResponse>
+    @GET("api/v1/json/1/lookupteam.php")
+    fun getTeamBadge(@Query("id") id: String): Deferred<TeamResponse>
 
-    @GET ("api/v1/json/1/lookupevent.php?id={id}")
-    fun getMatchDetails(@Path("id") id: String): Deferred<MatchResponse>
+    @GET("api/v1/json/1/lookupevent.php")
+    fun getMatchDetails(@Query("id") id: String): Deferred<MatchResponse>
 }
